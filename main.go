8d9a45426a7064a3b4c0b42d70cdbeb19c0b29f4
Marianne Feng <mariannefeng@level11.com>
2017-11-05 17:15:25 -0800

    added test movies file and changed file object to be map

--- main.go
+++ main.go
@@ -18,97 +18,96 @@ Request:
 package main
 
 import (
-    // "fmt"
-    "github.com/golang/protobuf/jsonpb"
-    "github.com/golang/protobuf/ptypes"
-    "github.com/google/uuid"
-    "github.com/gorilla/mux"
-    "github.com/urfave/negroni"
-    "log"
-    "net/http"
-    "os"
-    "io/ioutil"
-    "fmt"
-    "github.com/golang/protobuf/jsonpb"
-    "bytes"
+	// "fmt"
+	"bytes"
+	"fmt"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
 )
 
-const FileLocation = "file.json"
+const FileLocation = "movies.json"
+
 var unmarshaler = &jsonpb.Unmarshaler{}
 
 func AddRequest(w http.ResponseWriter, r *http.Request) {
-    // todo: validate against the movie database, tmdb when you get a api key
-    rq := PlexMovieRequest{}
-    unmarshaler := &jsonpb.Unmarshaler{
-        AllowUnknownFields: true,
-    }
-    if err := unmarshaler.Unmarshal(r.Body, &rq); err != nil {
-        log.Println("Can't unmarshal object!", err)
-        // return error summary and erro code
-    }
-    // validate
-    if err := Validate(&rq); err != nil {
-        log.Println("Missed Validations")
-        //  return missing fields
-    }
-    // once validated, add timestamp and uuid
-    rq.Uuid = uuid.New().String()
-    rq.TimeRequested = ptypes.TimestampNow()
-    w.Write([]byte("hi"))
+	// todo: validate against the movie database, tmdb when you get a api key
+	rq := PlexMovieRequest{}
+	unmarshaler := &jsonpb.Unmarshaler{
+		AllowUnknownFields: true,
+	}
+	if err := unmarshaler.Unmarshal(r.Body, &rq); err != nil {
+		log.Println("Can't unmarshal object!", err)
+		// return error summary and erro code
+	}
+	// validate
+	if err := Validate(&rq); err != nil {
+		log.Println("Missed Validations")
+		//  return missing fields
+	}
+	// once validated, add timestamp and uuid
+	rq.Uuid = uuid.New().String()
+	rq.TimeRequested = ptypes.TimestampNow()
+	w.Write([]byte("hi"))
 
 }
 
 func Validate(movieRequest *PlexMovieRequest) error {
-    // fmt.Println(movieRequest)
-    // validate w/ movie database
-    return nil
+	// fmt.Println(movieRequest)
+	// validate w/ movie database
+	return nil
 }
 
-
 func SubtractRequest(w http.ResponseWriter, r *http.Request) {
-    params := mux.Vars(r)
-    itemId := params["id"]
+	params := mux.Vars(r)
+	itemId := params["id"]
 
-    fileData, err := ioutil.ReadFile(FileLocation) // just pass the file name
+	fileData, err := ioutil.ReadFile(FileLocation) // just pass the file name
 
-    if err != nil {
-        fmt.Print(err)
-    }
+	if err != nil {
+		fmt.Print(err)
+	}
 
-    byteReader := bytes.NewReader(fileData)
-    currentReqs := &RequestList{}
+	byteReader := bytes.NewReader(fileData)
+	currentReqs := *RequestList{}
 
-    if err := unmarshaler.Unmarshal(byteReader, currentReqs); err != nil {
-        fmt.Print(err)
-    }
+	if err := unmarshaler.Unmarshal(byteReader, currentReqs); err != nil {
+		fmt.Print(err)
+	}
 
-    //for index, item := range people {
-    //if item.ID == params["id"] {
-    //people = append(people[:index], people[index+1:]...)
-    //break
-    //}
-    //json.NewEncoder(w).Encode(people)
-    //}
+	//for index, item := range people {
+	//if item.ID == params["id"] {
+	//people = append(people[:index], people[index+1:]...)
+	//break
+	//}
+	//json.NewEncoder(w).Encode(people)
+	//}
 
 }
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
-    mux := mux.NewRouter()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	mux := mux.NewRouter()
 
-    //TODO: validate adding input with tmdb api
-    mux.HandleFunc("/add", AddRequest).Methods("POST")
+	//TODO: validate adding input with tmdb api
+	mux.HandleFunc("/add", AddRequest).Methods("POST")
 
-    mux.HandleFunc("/subtract/{id}", SubtractRequest).Methods("DELETE")
+	mux.HandleFunc("/subtract/{id}", SubtractRequest).Methods("DELETE")
 
-    //TODO: email or text notification that something new got added?
-    // mux.HandleFunc("/finishhim")
+	//TODO: email or text notification that something new got added?
+	// mux.HandleFunc("/finishhim")
 
-    n := negroni.Classic()
-    n.UseHandler(mux)
-    n.Run(":" + port)
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	n.Run(":" + port)
 
 }
